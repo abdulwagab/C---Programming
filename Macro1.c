87c1557e6b796f3cb8956f60de07d6f5a30ef536
abdulwagab <65432634+abdulwagab@users.noreply.github.com>
2020-11-04 10:18:15 +0900

    Add files via upload

---
+++ Macro1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define BUFFER 250
+#define SIZE 100
+#define ABDUL (BUFFER+SIZE)
+							
+
+int main(void)
+{
+	/* Calling Simple Macro definition (Addition)*/
+	printf("%d \n", ABDUL);
+	return 0;
+}
